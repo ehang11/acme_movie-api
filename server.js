72170dadc6581cb4b977e69391a0a02574a94698
Eric Hang <ehang11@gmail.com>
2022-02-02 20:15:06 -0500

    add travis yml

--- server.js
+++ server.js
@@ -1,5 +1,7 @@
 const app = require('./app')
-
 const port = process.env.PORT || 3000
-
+const {
+  syncAndSeed,
+  models: { Actor, Movie, Role },
+} = require('./db')
 app.listen(port, () => console.log(`listening on port ${port}`))
